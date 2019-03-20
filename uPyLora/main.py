34200846ea5c448a6a648022646f9edc00a6b89f
foldedtoad <foldedtoad@gmail.com>
2019-03-20 15:10:04 -0700

    fix typo

--- uPyLora/main.py
+++ uPyLora/main.py
@@ -8,11 +8,10 @@ from controller_nrf52 import nRF52Controller
 
 
 controller = nRF52Controller()
+
 lora = controller.add_transceiver(SX127x(name = 'LoRa'),
                                   pin_id_ss     = nRF52Controller.PIN_ID_FOR_LORA_SS,
-                                  pin_id_RxDone = ESP32Controller.PIN_ID_FOR_LORA_DIO0)
-
-
+                                  pin_id_RxDone = nRF52Controller.PIN_ID_FOR_LORA_DIO0)
 
 #LoRaDuplexCallback.duplexCallback(lora)
 LoRaPingPong.ping_pong(lora)
